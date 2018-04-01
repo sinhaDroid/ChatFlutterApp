e2c0821c2f35c306e59bf82978442495d5891eb9
Deepanshu Sinha <107sinha@gmail.com>
2018-04-01 23:34:54 +0530

    added list to show messages

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textEditingController =
       new TextEditingController();
 
@@ -60,22 +61,48 @@ class ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Chit Chat")),
-      body: _buildTextComposer(),
-    );
+        appBar: new AppBar(title: new Text("Chit Chat")),
+        body: new Column(
+          children: <Widget>[
+            new Flexible(
+                child: new ListView.builder(
+                  padding: new EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, int index) => _messages[index],
+                  itemCount: _messages.length,
+                )
+            ),
+            new Divider(
+              height: 1.0,
+            ),
+            new Container(
+              decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: _buildTextComposer(),
+            )
+          ],
+        ));
   }
 
   void _handleSubmitted(String value) {
     _textEditingController.clear();
+    ChatMessage chatMessage = new ChatMessage(
+      text: value,
+    );
+    setState(() {
+      _messages.insert(0, chatMessage);
+    });
   }
 }
 
 class ChatMessage extends StatelessWidget {
   ChatMessage({this.text});
+
   final String text;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new Container(
       margin: const EdgeInsets.symmetric(vertical: 10.0),
       child: new Row(
@@ -83,12 +110,17 @@ class ChatMessage extends StatelessWidget {
         children: <Widget>[
           new Container(
             margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0]),),
+            child: new CircleAvatar(
+              child: new Text(_name[0]),
+            ),
           ),
           new Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              new Text(_name, style: Theme.of(context).textTheme.subhead,),
+              new Text(
+                _name,
+                style: Theme.of(context).textTheme.subhead,
+              ),
               new Container(
                 margin: const EdgeInsets.only(top: 5.0),
                 child: new Text(text),
@@ -100,3 +132,5 @@ class ChatMessage extends StatelessWidget {
     );
   }
 }
+
+const String _name = "Your Name";
