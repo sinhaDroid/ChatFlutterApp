265978cc24ca9c9dc842634da04018271596eaa8
Deepanshu Sinha <107sinha@gmail.com>
2018-04-08 15:13:36 +0530

    Chit Chat Basic Completed

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class ChitChatApp extends StatelessWidget {
 
 class ChatMessage extends StatelessWidget {
   ChatMessage({this.text, this.animationController});
+
   final String text;
   final AnimationController animationController;
 
@@ -75,7 +76,6 @@ class ChatMessage extends StatelessWidget {
   }
 }
 
-
 class ChatScreen extends StatefulWidget {
   @override
   State createState() => new ChatScreenState();
@@ -88,72 +88,84 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   bool _isComposing = false;
 
   Widget _buildTextComposer() {
-    return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new Row(
-        children: <Widget>[
-          new Flexible(
-            child: new TextField(
-              controller: _textEditingController,
-              onChanged: (String text) {
-                setState(() {
-                  _isComposing = text.length > 0;
-                });
-              },
-              onSubmitted: _handleSubmitted,
-              decoration:
-                  new InputDecoration.collapsed(hintText: "Send a message"),
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: new Row(children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textEditingController,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
             ),
-          ),
-          new Container(
-            margin: const EdgeInsets.symmetric(horizontal: 4.0),
-            child: Theme.of(context).platform == TargetPlatform.iOS
-                ? new CupertinoButton()
-                : new IconButton(
-                    icon: new Icon(Icons.send),
-                    onPressed: _isComposing
-                        ? () => _handleSubmitted(_textEditingController.text)
-                        : null),
-          )
-        ],
-      ),
+            new Container(
+                margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? new CupertinoButton(
+                        child: new Text("Send"),
+                        onPressed: _isComposing
+                            ? () =>
+                                _handleSubmitted(_textEditingController.text)
+                            : null,
+                      )
+                    : new IconButton(
+                        icon: new Icon(Icons.send),
+                        onPressed: _isComposing
+                            ? () =>
+                                _handleSubmitted(_textEditingController.text)
+                            : null,
+                      )),
+          ]),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? new BoxDecoration(
+                  border:
+                      new Border(top: new BorderSide(color: Colors.grey[200])))
+              : null),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Chit Chat"),
-          elevation:
-              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
-        ),
-        body: new Container(
-            child: new Column(
-              children: <Widget>[
-                new Flexible(
-                    child: new ListView.builder(
-                  padding: new EdgeInsets.all(8.0),
-                  reverse: true,
-                  itemBuilder: (_, int index) => _messages[index],
-                  itemCount: _messages.length,
-                )),
-                new Divider(
-                  height: 1.0,
+      appBar: new AppBar(
+        title: new Text("Chit Chat"),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body: new Container(
+          child: new Column(
+            children: <Widget>[
+              new Flexible(
+                  child: new ListView.builder(
+                padding: new EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (_, int index) => _messages[index],
+                itemCount: _messages.length,
+              )),
+              new Divider(height: 1.0),
+              new Container(
+                decoration: new BoxDecoration(
+                  color: Theme.of(context).cardColor,
                 ),
-                new Container(
-                  decoration: new BoxDecoration(
-                    color: Theme.of(context).cardColor,
+                child: _buildTextComposer(),
+              )
+            ],
+          ),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? new BoxDecoration(
+                  border: new Border(
+                    top: new BorderSide(color: Colors.grey[200]),
                   ),
-                  child: _buildTextComposer(),
                 )
-              ],
-            ),
-            decoration: Theme.of(context).platform == TargetPlatform.iOS
-                ? new BoxDecoration(
-                    border: new Border(
-                        top: new BorderSide(color: Colors.grey[200])))
-                : null));
+              : null),
+    );
   }
 
   void _handleSubmitted(String value) {
